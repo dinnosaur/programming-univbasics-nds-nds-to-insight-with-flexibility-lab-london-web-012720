f95d142bd61c81b8327497b699152d2d50e94428
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-27 15:05:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -161,11 +161,11 @@ puts director1
       inner_index = 0
 
       while inner_index < source[outer_index][:movies].length do
-                 hash[:title] = []
+                # hash[:title] = []
               hash[:title] = source[outer_index][:movies][inner_index][:title]
-                   hash[:director_name]=[]
+                #  hash[:director_name]=[]
                       hash[:director_name] = source[outer_index][:name]
-
+hash1 = [hash]
 #      if outer_index < 1
 #          director1 << hash.dup
 # else
